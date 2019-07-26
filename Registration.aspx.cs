8e13cb4e41bf76394bd52b9ccf790a6ee3fc0c43
luel-4013 <luelworkneh@gmail.com>
2019-07-25 22:49:46 -0800

    Athena Resort and spa(web based)
    Resort Management system

---
+++ Registration.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        MySql.Data.MySqlClient.MySqlConnection conn;
+        MySql.Data.MySqlClient.MySqlCommand cmd;
+        String queryStr;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            vvcvghbnjmknbvcvbhjnm
+        }
+        protected void registerEventMethod(object sender, EventArgs e)
+        {
+            registerUser();
+        }
+        private void registerUser()
+        {
+            String connString = System.Configuration.ConfigurationManager.ConnectionStrings["WebConnString"].ToString();
+             conn = new MySql.Data.MySqlClient.MySqlConnection(connString);
+            conn.Open();
+            queryStr = "INSERT INTO misge.reserve(Fname,Lname, Email,Number,NumP,arrTime )" + "VALUES('" + firstnameTextBox.Text + "','" + lastnameTextBox.Text + "','" + emailTextBox.Text + "','" + numberTextBox.Text + "','" + numTextBox1.Text + "','" + arrTextBox.Text + "')";
+           
+            cmd = new MySql.Data.MySqlClient.MySqlCommand(queryStr, conn);
+
+            cmd.ExecuteReader();nnnnnnnnnnnnnnnn
+            conn.Close();
+        }
+    }
+}  
---
+++ WebForm1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.486323519223!2d38.804590214251725!3d9.019322593527594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b8544cdbd67eb%3A0x22240d984fbdd4c6!2sUnnamed+Road%2C+Addis+Ababa!5e0!3m2!1sen!2set!4v1526216085938" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    
+    </div>
+    </form>
+</body>
+</html>
